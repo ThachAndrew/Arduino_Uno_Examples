e4ef308bd9a7654de03bb5f94c4a6348ba24cdaa
Andrew Thach <andrew.thach123@gmail.com>
2024-08-18 15:10:44 -0700

    Updated pins to be compatible with Mike's circuit tester.

--- ForceSensingResistor/ForceSensingResistor.ino
+++ ForceSensingResistor/ForceSensingResistor.ino
@@ -1,9 +1,11 @@
+const int PRESSURE_SENSOR = A1;
+
 void setup() {   
     Serial.begin(9600);
 } 
 
 void loop() {
-    int reading = analogRead(A0);
+    int reading = analogRead(PRESSURE_SENSOR);
     Serial.println("Analog Reading is: " + String(reading));  
     delay(10);
 }
