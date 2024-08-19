a1fa27faad70613c39afd09e7baff97b030cccf5
Andrew Thach <andrew.thach123@gmail.com>
2024-08-19 01:35:34 -0700

    Added second button.

---
+++ ButtonBlinkFrequency/ButtonBlinkFrequency.ino
@@ -0,0 +1,25 @@
+// TODO: Add second button, and implement blink frequency.
+
+const int LED_0 = 13;
+const int BUTTON_0 = 2;
+int button_state = 0;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(LED_0, OUTPUT);
+    pinMode(BUTTON_0, INPUT_PULLUP);
+}
+
+void loop() {
+    button_state = digitalRead(BUTTON_0);
+    Serial.println("Button state is: " + String(button_state));
+
+    if (button_state == 1) {
+        digitalWrite(LED_0, LOW);
+    }
+
+    if (button_state == 0) {
+        digitalWrite(LED_0, HIGH);
+    }
+}
+
