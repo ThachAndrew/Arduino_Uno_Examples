563bd61a34ffa43f11484c174a77289d7a263676
Andrew Thach <andrew.thach123@gmail.com>
2024-09-05 00:54:33 -0700

    Implemented a 2 button incrementer/decrementer with debounce.

--- BinaryLEDs/BinaryLEDs.ino
+++ BinaryLEDs/BinaryLEDs.ino
@@ -1,5 +1,6 @@
 /*
   Converts user integer input for to display as binary on 4 LEDs.
+  Implements buttons (with debounce) which decrements/increments the display.
 */
 
 
@@ -7,8 +8,12 @@
 int led_pins[4] = {2, 4, 7, 8};
 // Keeps track of which LED to be turned on.
 int index = 0;
-int user_input;
+int user_input; //FIXME: Consider moving this variable to a different scope.
 
+int last_state_button_0 = HIGH;
+int last_state_button_1 = HIGH;
+
+// Displays a number as 4 digit LED binary.
 void displayBinaryLED(int num) {
     for (int i = 0; i < 4; ++i)
         digitalWrite(led_pins[i], bitRead(num, i));
@@ -22,6 +27,9 @@ void setup() {
     pinMode(led_pins[2], OUTPUT);
     pinMode(led_pins[3], OUTPUT);
 
+    pinMode(12, INPUT_PULLUP);
+    pinMode(13, INPUT_PULLUP);
+
     Serial.print("What number do you want displayed? ");
     while(!Serial.available()) {}
 
@@ -30,4 +38,24 @@ void setup() {
 
 void loop() {
     displayBinaryLED(user_input);
+
+    // Remember last state so the button debounce knows when it was lifted.
+    if (last_state_button_0 == LOW && digitalRead(12) == HIGH) {
+        last_state_button_0 = HIGH;
+    }
+
+    // Button debounce, to counteract a "held button" performing unintentional control.
+    if (last_state_button_0 == HIGH && digitalRead(12) == LOW) {
+        user_input--;
+        last_state_button_0 = LOW;
+    }
+
+    if (last_state_button_1 == LOW && digitalRead(13) == HIGH) {
+        last_state_button_1 = HIGH;
+    }
+
+    if (last_state_button_1 == HIGH && digitalRead(13) == LOW) {
+        user_input++;
+        last_state_button_1 = LOW;
+    }
 }
